ba91463869e059b678da87e9f025ed633a171cf9
Arno Slabbinck <arno.slabbinck@hotmail.com>
2024-02-27 08:35:17 +0100

    Project cache and assembly

--- frontorigins.Shared/obj/Release/net8.0/frontorigins.Shared.AssemblyInfo.cs
+++ frontorigins.Shared/obj/Release/net8.0/frontorigins.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontorigins.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d8dc686637d08d9a5487932721eacb7c404e4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontorigins.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontorigins.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
