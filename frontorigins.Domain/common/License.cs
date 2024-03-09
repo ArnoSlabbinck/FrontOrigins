1a2ef076fb4dc6d25ae54a31c248986b4aa66735
Arno Slabbinck <arno.slabbinck@hotmail.com>
2024-03-09 20:59:59 +0100

    add enum license and workstatus

---
+++ frontorigins.Domain/common/License.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontorigins.Domain.common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace frontorigins.Domain.common
+    {
+        public enum License
+        {
+            AM,
+            A1,
+            B,
+            C,
+            None,
+
+        }
+    }
+}
---
+++ frontorigins.Domain/common/WorkStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontorigins.Domain.common
+{
+    public enum WorkStatus
+    {
+        [Description("Fulltime Employment")]
+        Fulltime,
+        [Description("Parttime Employment")]
+        Parttime
+    }
+}
