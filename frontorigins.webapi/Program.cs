33894400afa18d2863461fd405e12e55689ebf43
Arno Slabbinck <arno.slabbinck@hotmail.com>
2024-03-09 20:10:36 +0100

    add database setup add DbContext  and database connection localhost

--- frontorigins.webapi/Program.cs
+++ frontorigins.webapi/Program.cs
@@ -1,3 +1,7 @@
+using frontorigins.Infrastructure.Connection;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -16,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -23,3 +34,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
