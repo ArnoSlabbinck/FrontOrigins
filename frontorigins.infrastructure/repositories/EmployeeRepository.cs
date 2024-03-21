23e191d8f225a5bb7af26a45865378eee692827b
Arno Slabbinck <arno.slabbinck@hotmail.com>
2024-03-21 17:47:10 +0100

    Added Employee respository

---
+++ frontorigins.infrastructure/repositories/EmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using frontorigins.Domain.entities;
+using frontorigins.Infrastructure.database;
+using frontorigins.Infrastructure.unitofwork;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontorigins.Infrastructure.repositories
+{
+    public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(DbContext dbContext, IUnitOfWork unitOfWork) : base(dbContext, unitOfWork)
+        {
+        }
+    }
+}
