27486df69c9944e71c2c683d5600264723f3acef
Arno Slabbinck <arno.slabbinck@hotmail.com>
2024-03-21 17:48:56 +0100

    Added Updated Address

--- frontorigins.Domain/aggregates/Address.cs
+++ frontorigins.Domain/aggregates/Address.cs
@@ -1,14 +1,23 @@
 ﻿using frontorigins.Domain.common;
+using System.ComponentModel.DataAnnotations;
 
 namespace frontorigins.Domain.aggregates
 {
     public record Address 
     {
+        [Display(Name = "StreetName")]
         public string streetAddress { get; private set; }
+
+        [Display(Name = "City")]
         public string city { get; private set; }
+
+        [Display(Name = "State")]
         public string state { get; private set; }
+
+        [Display(Name = "Zipcode")]
         public string zipCode { get; private set; }
 
+        [Display(Name = "Country")]
         public string country { get; private set; }
         public Address(string streetAddress, string city, string state, string zipCode, string country)
         {
