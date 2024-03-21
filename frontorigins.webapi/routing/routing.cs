3d053cc85e40be3fd23d7d6625d20bd88670b327
Arno Slabbinck <arno.slabbinck@hotmail.com>
2024-03-21 17:52:00 +0100

    Added Routing naming

---
+++ frontorigins.webapi/routing/routing.cs
@@ -0,0 +1,16 @@
+﻿
+namespace frontorigins.webapi.routing
+{
+    public static class Routing
+    {
+        public const string V1 = "v1/Api";
+
+
+        public const string Employee = V1 + "/Employee" ;
+        public const string EmployeeHome = Employee + "/home";
+    }
+    
+}
+
+
+
