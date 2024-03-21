9bb2e8c7c429b077643f3609b3effd7e7839efad
Arno Slabbinck <arno.slabbinck@hotmail.com>
2024-03-21 17:51:08 +0100

    Added Employee Controller

---
+++ frontorigins.webapi/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using frontorigins.Application.services;
+using frontorigins.Domain.entities;
+using frontorigins.webapi.routing;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace frontorigins.webapi.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route(Routing.Employee)]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        private IEmployeeService EmployeeService;
+        private IMapper mapper;
+        private readonly DbContext context;
+        public EmployeeController(DbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        [HttpGet(Routing.EmployeeHome)]
+        public IEnumerable<Employee> Index()
+        {
+            return null;
+
+        }
+    }
+}
